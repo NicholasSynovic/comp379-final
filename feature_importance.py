f1e40033afba5a905a818ee6e14ce8bf9d9ea383
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-12-15 11:25:56 -0600

    Format and clean python files

--- feature_importance.py
+++ feature_importance.py
@@ -1,27 +1,27 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
-import matplotlib.pyplot as plt
 
 df = pd.read_csv("./Crimes_-_2001_to_Present.csv")
-df.columns = [c.lower().replace(' ', '_') for c in df.columns]
+df.columns = [c.lower().replace(" ", "_") for c in df.columns]
 df = df.dropna()
 df = df.reset_index(drop=True)
-df = df.drop(['date', 'latitude', 'longitude', 'location'], axis = 1)
+df = df.drop(["date", "latitude", "longitude", "location"], axis=1)
 
 le = LabelEncoder()
-df['iucr'] = le.fit_transform(df['iucr'])
-df['primary_type'] = le.fit_transform(df['primary_type'])
-df['description'] = le.fit_transform(df['description'])
-df['location_description'] = le.fit_transform(df['location_description'])
-df['fbi_code'] = le.fit_transform(df['fbi_code'])
+df["iucr"] = le.fit_transform(df["iucr"])
+df["primary_type"] = le.fit_transform(df["primary_type"])
+df["description"] = le.fit_transform(df["description"])
+df["location_description"] = le.fit_transform(df["location_description"])
+df["fbi_code"] = le.fit_transform(df["fbi_code"])
 
-y_train = df['arrest']
-X_train = df.drop('arrest', axis = 1)
+y_train = df["arrest"]
+X_train = df.drop("arrest", axis=1)
 feat_labels = X_train.columns
 
-forest = RandomForestClassifier(n_estimators = 500, random_state = 1)
+forest = RandomForestClassifier(n_estimators=500, random_state=1)
 forest.fit(X_train, y_train)
 
 importances = forest.feature_importances_
@@ -29,24 +29,26 @@ importances = forest.feature_importances_
 indices = np.argsort(importances)[::-1]
 
 for f in range(X_train.shape[1]):
-    print("%2d) %-*s %f" % (f + 1, 30,
-                            feat_labels[indices[f]],
-                            importances[indices[f]]))
+    print(
+        "%2d) %-*s %f" % (f + 1, 30, feat_labels[indices[f]], importances[indices[f]])
+    )
 
-plt.title('Feature Importance')
-plt.bar(range(X_train.shape[1]),
-        importances[indices],
-        align = 'center')
+plt.title("Feature Importance")
+plt.bar(range(X_train.shape[1]), importances[indices], align="center")
 
 for i in range(X_train.shape[1]):
-    plt.text(i, round(importances[indices[i]], 3), round(importances[indices[i]], 3), ha = 'center')
+    plt.text(
+        i,
+        round(importances[indices[i]], 3),
+        round(importances[indices[i]], 3),
+        ha="center",
+    )
 
-plt.xticks(range(X_train.shape[1]),
-            feat_labels[indices], rotation = 90)
+plt.xticks(range(X_train.shape[1]), feat_labels[indices], rotation=90)
 plt.xlim([-1, X_train.shape[1]])
 
-plt.xlabel('Features')
-plt.ylabel('Importance Score')
+plt.xlabel("Features")
+plt.ylabel("Importance Score")
 
 plt.tight_layout()
 plt.show()
