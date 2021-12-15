31e07637b5d79a080feedc9e2bffe6d4513f7229
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-12-15 13:12:12 -0600

    Rename file

--- clusterCrimes.py
+++ clusterCrimes.py
@@ -1,20 +1,40 @@
 """
 Chicago Crimes Dataset, Kmeans clustering
 """
+from matplotlib.collections import PathCollection
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from pandas.core.frame import DataFrame
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import LabelEncoder
 
+from argparse import ArgumentParser, Namespace
 
-def preprocess_(data):
-    df = pd.read_csv(data)
+
+def parseArgs() -> Namespace:
+    parser: ArgumentParser = ArgumentParser(
+        prog="Cluster Crimes",
+        usage="Execute K-Means clustering on a Chicago Crimes Dataset",
+        description="Datasets can be downloaded from: https://data.cityofchicago.org/Public-Safety/Crimes-2001-to-Present/ijzp-q8t2/data",
+    )
+    parser.add_argument(
+        "-d",
+        "--dataset",
+        type=str,
+        required=True,
+        help="Dataset filename",
+    )
+    return parser.parse_args()
+
+
+def preprocessDataset(dataset:str)    -> DataFrame:
+    df: DataFrame = pd.read_csv(dataset)
     # remove space from column names
     df.columns = [c.lower().replace(" ", "_") for c in df.columns]
     df = df.dropna()
     df = df.reset_index(drop=True)
-    le = LabelEncoder()
+    le: LabelEncoder = LabelEncoder()
     df["iucr"] = le.fit_transform(df["iucr"])
     df["primary_type"] = le.fit_transform(df["primary_type"])
     df["description"] = le.fit_transform(df["description"])
@@ -23,10 +43,11 @@ def preprocess_(data):
 
 
 # visualize the clusters, 2d for now, and 3 clusters
-def visualize_clusters(label, df, col1, col2):
-    u_labels = np.unique(label)  # get unique labels
-    sc = 0
+def _visualizeClusters(label, df, col1, col2, output:str) -> PathCollection:
+    u_labels: np.ndarray = np.unique(label)  # get unique labels
+    sc:int = 0
 
+    i: str
     for i in u_labels:
         cls = df[label == i]
         sc = plt.scatter(
@@ -39,7 +60,6 @@ def visualize_clusters(label, df, col1, col2):
     plt.ylabel(col2)
 
     # plt.imshow(depth_)
-    output = col1 + "_" + col2
     plt.savefig(output)
     # plt.show()
     # save for later
@@ -47,18 +67,22 @@ def visualize_clusters(label, df, col1, col2):
 
 
 # labels = array of columns
-def run_k_means(labels, kmeans, df):
+def fitKMeans(labels, kmeans, df):
     label = kmeans.fit_predict(df[labels])
     u_labels = np.unique(label)
-    visualize_clusters(label, df, labels[0], labels[1])  # only 2d
+    _visualizeClusters(label, df, labels[0], labels[1], output="output.png")  # only 2d
 
 
 def main():
-    df = preprocess_("Crimes_-_2001_to_Present.csv")
-    kmeans = KMeans(n_clusters=3, random_state=0)
+    args: Namespace = parseArgs()
+
+    df: DataFrame = preprocessDataset(args.dataset)
+
+    kmeans:KMeans = KMeans(n_clusters=3, random_state=0)
+
     # run_k_means(['district', 'community_area'], kmeans, df)
     # run_k_means(['primary_type', 'community_area'], kmeans, df)
-    run_k_means(["primary_type", "description"], kmeans, df)
+    fitKMeans(["primary_type", "description"], kmeans, df)
 
 
 if __name__ == "__main__":
